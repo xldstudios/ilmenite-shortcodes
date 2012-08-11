092a9f9eba27345c38dc42ed7cecfcde1c45b021
Erik Bernskiold <erik@bernskioldmedia.com>
2012-08-11 22:33:35 +0200

    Plugin Header Information
    + added plugin header information
    
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

---
+++ ilmenite-shortcodes.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Ilmenite Shortcodes
+Plugin URI: http://www.xldstudios.com/plugins/ilmenite-shortcodes/
+Description: A standard set of shortcodes.
+Version: 0.1
+Author: XLD Studios
+Author URI: http://www.xldstudios.com
+License: GPL2
+*/
