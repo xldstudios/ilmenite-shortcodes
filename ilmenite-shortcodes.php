dca2368789870e72a51a1d8bb1c29460faf94724
Erik Bernskiold <erik@xldstudios.com>
2013-07-19 12:06:38 +0200

    Changed plugin architecture
    Signed-off-by: Erik Bernskiold <erik@xldstudios.com>

--- ilmenite-shortcodes.php
+++ ilmenite-shortcodes.php
@@ -1,81 +1,121 @@
 <?php
-/*
-Plugin Name: Ilmenite Shortcodes
-Plugin URI: http://www.xldstudios.com/plugins/ilmenite-shortcodes/
-Description: A standard set of shortcodes.
-Version: 1.1
-Author: XLD Studios
-Author URI: http://www.xldstudios.com
-License: GPL2
-*/
+/**
+ * Plugin Name: Ilmenite Shortcodes
+ * Plugin URI: http://www.xldstudios.com/
+ * Author: XLD Studios
+ * Author URI: http://www.xldstudios.com/
+ * Version: 1.2
+ * Text Domain: ilshortcodes
+ * License: GPL2
+ */
 
 /**
- * Plugin Definitions
- **/
+ * Get some constants ready for paths when your plugin grows
+ */
+define( 'ILS_VERSION', '1.2' );
+define( 'ILS_PATH', dirname( __FILE__ ) );
+define( 'ILS_PATH_INCLUDES', dirname( __FILE__ ) . '/inc' );
+define( 'ILS_FOLDER', basename( ILS_PATH ) );
+define( 'ILS_URL', plugins_url() . '/' . ILS_FOLDER );
+define( 'ILS_URL_INCLUDES', ILS_URL . '/inc' );
 
-define( 'IS_VERSION', '1.0' );
-
-define( 'IS_PLUGIN_PATH', dirname( __FILE__ ) );
-define( 'IS_PLUGIN_URL', plugins_url( '', __FILE__ ) );
-define( 'IS_PLUGIN_FILE', plugin_basename( __FILE__ ) );
-
-define( 'IS_PLUGIN_INC', IS_PLUGIN_PATH . '/inc' );
-define( 'IS_SHORTCODES', IS_PLUGIN_PATH . '/shortcodes' );
-
-define( 'IS_ASSETS', IS_PLUGIN_URL . '/assets' );
-define( 'IS_CSS', IS_ASSETS . '/css' );
-define( 'IS_IMAGES', IS_ASSETS . '/images' );
-define( 'IS_JS', IS_ASSETS . '/js' );
 
 /**
- * Set up Plugin
  *
- * Loads all the plugin files and calls when appropriate.
- **/
+ * The plugin base class - the root of all WP goods!
+ *
+ * @author nofearinc
+ *
+ */
+class Ilmenite_Shortcodes {
 
-require_once( IS_PLUGIN_INC . '/plugin.php' ); // Include base file
-require_once( IS_PLUGIN_INC . '/config.php' ); // Get config file
+	/**
+	 *
+	 * Assign everything as a call from within the constructor
+	 */
+	function __construct() {
 
-$class_name = 'IS_';
+		// Add scripts and styles
+		add_action( 'wp_enqueue_scripts', array( $this, 'ils_add_JS' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'ils_add_CSS' ) );
 
-// Load admin file if in the admin section
-if( is_admin() ) {
-	$class_name .= 'Admin';
-	require_once( IS_PLUGIN_INC . '/admin.php' );
-} else {
-	$class_name .= 'Public';
-	require_once( IS_PLUGIN_INC . '/theme-functions.php' );
-	require_once( IS_PLUGIN_INC . '/public.php' );
+		// Register activation and deactivation hooks
+		register_activation_hook( __FILE__, array( $this, 'ils_on_activate_callback' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'ils_on_deactivate_callback' ) );
+
+		// Make shortcodes available
+		add_action( 'init', array( $this, 'ils_shortcodes' ) );
+
+		// Add the textdomain and support translation
+		add_action( 'plugins_loaded', array( $this, 'ils_add_textdomain' ) );
+	}
+
+	/**
+	 * Adding JavaScript scripts
+	 */
+	function ils_add_JS() {
+
+		// Make sure jQuery is loaded
+		wp_enqueue_script( 'jquery' );
+
+	}
+
+	/**
+	 * Add CSS styles
+	 */
+	function ils_add_CSS() {
+
+		// Register Shortcodes CSS
+		wp_register_style( 'ilmenite-shortcodes', plugins_url( '/assets/css/shortcodes.css', __FILE__ ), false, ILS_VERSION, 'all' );
+
+		// Enqueue Styles
+		wp_enqueue_style( 'ilmenite-shortcodes' );
+	}
+
+	/**
+	 * Register activation hook
+	 */
+	function ils_on_activate_callback() {
+		// do something on activation
+	}
+
+	/**
+	 * Register deactivation hook
+	 */
+	function ils_on_deactivate_callback() {
+		// do something when deactivated
+	}
+
+	/**
+	 * Load Shortcodes
+	 */
+	function ils_shortcodes() {
+
+		// Buttons Shortcode
+		require_once( ILS_PATH_INCLUDES . '/shortcodes/buttons.php' );
+
+		// Code Shortcode
+		require_once( ILS_PATH_INCLUDES . '/shortcodes/code.php' );
+
+		// Columns Shortcode
+		require_once( ILS_PATH_INCLUDES . '/shortcodes/columns.php' );
+
+		// Divider Shortcode
+		require_once( ILS_PATH_INCLUDES . '/shortcodes/divider.php' );
+
+		// Intro Text Shortcode
+		require_once( ILS_PATH_INCLUDES . '/shortcodes/intro-text.php' );
+
+	}
+
+	/**
+	 * Add textdomain for plugin
+	 */
+	function ils_add_textdomain() {
+		load_plugin_textdomain( 'ilshortcodes', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
 }
 
-/**
- * Configuration Data
- **/
-
-$is_config_data = array(
-	'plugin_file' => IS_PLUGIN_FILE,
-	'version' => IS_VERSION
-);
-
-/**
- * Execute Plugin Class
- **/
-
-$ilmenite_shortcode = new $class_name( new IS_Config( $is_config_data ) );
-
-unset( $class_name, $is_config_data ); // Unset when done.
-
-/**
- * Auto Update
- **/
-add_action('init', 'is_auto_update');
-
-function is_auto_update() {
-	
-	require_once( IS_PLUGIN_INC . '/auto-update.php' ); // Load auto update class
-	
-	$remote_update_link = 'http://www.xldstudios.com/updates/plugins/ilmenite-shortcodes/update.php';
-	
-	new WP_Auto_Update( IS_VERSION, $remote_update_link, IS_PLUGIN_FILE );
-	
-}
+// Initialize everything
+$is_shortcodes = new Ilmenite_Shortcodes();
